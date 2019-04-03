e0071064c3a11c57a0a7dfabac4d075dc4781f24
Tor-Björn Fjellner <tobifjellner@users.noreply.github.com>
2019-04-03 11:24:54 +0200

    First draft of Swedish link collection in Swedish

---
+++ tutorials/hosting-specific/tutorials-en.sv
@@ -0,0 +1,29 @@
+# Specifika instruktioner för olika webbhotell (Svenska)
+
+
+[![Redigera listan](https://img.shields.io/badge/Edit_List--green.svg?style=social)](https://github.com/wp-core-php/servehappy-resources/edit/master/tutorials/hosting-specific/tutorials-sv.md)
+[![Lägg till nytt språk](https://img.shields.io/badge/Add_New_Language--green.svg?style=social)](https://github.com/wp-core-php/servehappy-resources/new/master/tutorials/hosting-specific)
+
+
+
+Det här är en lista med länkar till guider för hur man uppgraderar sin PHP-version hos olika specifika webbhotell på den svenska marknaden.
+
+* [Ballou](https://minasidor.ballou.se/knowledgebase/1021/PHP-version-Linux.html)
+* [Beebyte](https://docs.beebyte.se/pages/viewpage.action?pageId=1605635)
+* [Binero](https://www.binero.se/faq/faq-kontrollpanel/kontrollpanel/hur-kan-jag-i-kontrollpanelen-se-eller-redigera-min-php-version-f-r-min-webbplats)
+* [Crystone](https://helpdesk.crystone.se/hc/sv/articles/207525186-%C3%84ndra-PHP-version)
+* [FS Data](https://fsdata.se/manualer/kontrollpanel/hemsida/byt-php-version-genom-waff/)
+* [Loopia](https://support.loopia.se/wiki/uppgradera-php-hos-loopia/)
+* [Misshosting](https://missgroup.intercom.help/miss-hosting/video-guider/video-hur-jag-andrar-php-version) (på engelska)
+* [Oderland](https://www.oderland.se/support/artikel/hur-andrar-jag-php-version/)
+* [One](https://help.one.com/hc/sv/articles/115005585589-Vad-ska-jag-kontrollera-vid-uppgradering-av-PHP-)
+* [Seravo](https://help.seravo.com/en/knowledgebase/13-server-configuration/docs/107-transfer-your-site-to-use-newest-php-version) (på engelska)
+* [Servebolt](https://service.servebolt.com/server-setup-and-configuration/supported-php-versions) (på engelska)
+* [Surftown](https://controlpanel.surftown.com/knowledgebase/article/225/att-byta-till-php-7&languagechange=Swedish)
+* [Templ.io](länk saknas)
+* [Wopsa](länk saknas)
+* [Wpsyd](https://www.wpsyd.com/kb/view.php?id=107)
+* []()
+* []()
+* []()
+* []()
